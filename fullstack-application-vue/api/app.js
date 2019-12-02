d4bb3aba4806e2d42fab5854e6c848392a2c77de
yugasun <yuga_sun@163.com>
2019-12-02 10:17:57 +0800

    feat: add db connect for fullstack-application-vue

--- fullstack-application-vue/api/app.js
+++ fullstack-application-vue/api/app.js
@@ -1,14 +1,76 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
 
-const app = express()
+function initMysqlPool() {
+  const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } = process.env;
 
-app.use(cors())
+  const promisePool = mysql
+    .createPool({
+      host: DB_HOST,
+      user: DB_USER,
+      port: DB_PORT,
+      password: DB_PASSWORD,
+      database: DB_DATABASE,
+      connectionLimit: 1,
+    })
+    .promise();
+
+  return promisePool;
+}
+
+function jsonReturn(data) {
+  return JSON.stringify(data);
+}
+
+const app = express();
+
+if (!app.promisePool) {
+  app.promisePool = initMysqlPool();
+}
+
+app.use(bodyParser.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send(JSON.stringify({ message: `Server time: ${new Date().toString()}` }))
-})
+  res.send(jsonReturn({ message: `Server time: ${new Date().toString()}` }));
+});
 
-module.exports = app
+app.get('/users', async (req, res) => {
+  const [data] = await app.promisePool.query('select * from users');
+  res.send(
+    jsonReturn({
+      data: data,
+    }),
+  );
+});
+
+app.post('/users', async (req, res) => {
+  let result = '';
+  try {
+    const { name, email, site } = req.body;
+    const [res] = await app.promisePool.query('INSERT into users SET ?', {
+      name: name,
+      email: email,
+      site: site,
+    });
+    result = {
+      data: res && res.insertId,
+      message: 'Insert Success',
+    };
+  } catch (e) {
+    result = {
+      data: e,
+      message: 'Insert Fail',
+    };
+  }
+
+  res.send(jsonReturn(result));
+});
+
+module.exports = app;
--- fullstack-application-vue/dashboard/src/index.js
+++ fullstack-application-vue/dashboard/src/index.js
@@ -1,20 +1,44 @@
-'use strict'
+'use strict';
 
-require('../env')
+require('../env');
 
-const Vue = require('vue')
+const Vue = require('vue');
+const axios = require('axios');
 
 module.exports = new Vue({
   el: '#root',
   data: {
     message: 'Click me!',
-    isVisible: true
+    isVisible: true,
+    form: {
+      name: '',
+      email: '',
+      site: '',
+    },
+    userList: [],
   },
   methods: {
     async queryServer() {
-      const response = await fetch(window.env.apiUrl)
-      const result = await response.json()
-      this.message = result.message
-    }
+      const response = await fetch(window.env.apiUrl);
+      const result = await response.json();
+      this.message = result.message;
+    },
+
+    async getUsers() {
+      const res = await axios.get(window.env.apiUrl + 'users');
+      this.userList = res.data && res.data.data || [];
+    },
+
+    async addUser() {
+      const data = this.form;
+      const res = await axios.post(window.env.apiUrl + 'users', data);
+      console.log(res);
+      if (res.data) {
+        this.getUsers();
+      }
+    },
+  },
+  mounted() {
+    this.getUsers();
   }
-})
+});
