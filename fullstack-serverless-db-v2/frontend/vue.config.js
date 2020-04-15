a6dc8f133536501b246b36e0c0d7a1efc03b1f94
yugasun <yuga_sun@163.com>
2020-04-15 15:38:19 +0800

    feat(fullstack-serverless-db-v2): remove perender

--- fullstack-serverless-db-v2/frontend/vue.config.js
+++ fullstack-serverless-db-v2/frontend/vue.config.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-
 module.exports = {
   configureWebpack: {
     resolve: {
@@ -11,13 +8,6 @@ module.exports = {
     externals: {
       env: 'env'
     },
-    plugins: [
-      new PrerenderSPAPlugin({
-        // Required - The path to the webpack-outputted app to prerender.
-        staticDir: path.join(__dirname, 'dist'),
-        // Required - Routes to render.
-        routes: ['/'],
-      }),
-    ],
+    plugins: [],
   },
 };
